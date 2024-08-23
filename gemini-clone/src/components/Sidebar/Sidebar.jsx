7ff6f40f113f82e193a8836a3c89961c54d6764f
Mayank <mayanksingh6787@gmail.com>
2024-08-23 16:37:19 +0530

    Recent updated

--- gemini-clone/src/components/Sidebar/Sidebar.jsx
+++ gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -6,6 +6,11 @@ const Sidebar = () => {
   const [extended, setExtended] = useState(false);
   const { onSent, prevPrompt, setRecentPrompt } = useContext(Context);
 
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
+
   return (
     <div className="sidebar">
       <div className="top">
@@ -24,16 +29,12 @@ const Sidebar = () => {
             <p className="recent-title">Recent</p>
             {prevPrompt.map((item, index) => {
               return (
-                <div className="recent-entry">
+                <div onClick={() => loadPrompt(item)} className="recent-entry">
                   <img src={assets.message_icon} alt="" />
                   <p>{item.slice(0, 18)}...</p>
                 </div>
               );
             })}
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="" />
-              <p>What is react...</p>
-            </div>
           </div>
         ) : null}
       </div>
--- gemini-clone/src/context/Context.jsx
+++ gemini-clone/src/context/Context.jsx
@@ -32,9 +32,17 @@ const ContextProvider = (props) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
-    setRecentPrompt(input);
-    setPrevPrompt((prev) => [...prev, input]);
-    const response = await run(input);
+    
+    let response;
+    if (prompt !== undefined) {
+      response = await run(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPrevPrompt((prev) => [...prev, input]);
+      setRecentPrompt(input);
+      response = await run(input);
+    }
+
     let responseArray = response.split("**");
     let newResponse = "";
     for (let i = 0; i < responseArray.length; i++) {
