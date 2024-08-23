fa71beda440769569672c4b9f273959d1f96083d
Mayank <mayanksingh6787@gmail.com>
2024-08-23 16:20:54 +0530

    One of the last commit

---
+++ gemini-clone/src/config/gemini.js
@@ -0,0 +1,42 @@
+const API_KEY = "";
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = "AIzaSyBGXznRSV5TNBTiKxEcprrbc7gHI6ZCLqc";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
---
+++ gemini-clone/src/context/Context.jsx
@@ -0,0 +1,78 @@
+import { createContext, useState } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  //to save the input
+  const [input, setInput] = useState("");
+
+  //send button clicked then prompt gets saved
+  const [recentPrompt, setRecentPrompt] = useState("");
+
+  //input history
+  const [prevPrompt, setPrevPrompt] = useState("");
+
+  //Once this is true it will hide the boxes of main
+  const [showResult, setShowResult] = useState(false);
+
+  //Loading animation and then again turn it false it data obtained
+  const [loading, setLoading] = useState(false);
+
+  //Result is shown
+  const [resultData, setResultData] = useState("");
+
+  const delayPara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
+
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    setPrevPrompt((prev) => [...prev, input]);
+    const response = await run(input);
+    let responseArray = response.split("**");
+    let newResponse = "";
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+
+    let newResponse2 = newResponse.split("*").join("</br></br>");
+    let newResponseArray = newResponse2.split(" ");
+
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord + " ");
+    }
+    setLoading(false);
+    setInput("");
+  };
+
+  //props provider on the parent level to all the child components
+  const contextValue = {
+    prevPrompt,
+    setPrevPrompt,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
